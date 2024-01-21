949c46b0376b1eefb19626e494bcbcc1327fce84
Arunava-Barua <rahulbarua31@gmail.com>
2024-01-21 21:02:57 +0530

    facial signature

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import CameraCaptureComponent from './CameraCaptureComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <CameraCaptureComponent />
+    </div>
+  );
+}
+
+export default App;
